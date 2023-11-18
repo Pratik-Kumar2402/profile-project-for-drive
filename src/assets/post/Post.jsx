a446df90b374aea61da435bd20faf3d5e607e4e7
Pratik-Kumar2402 <pratik.kumar2402@gmail.com>
2023-11-18 11:56:12 +0530

    currently for big display done

--- src/assets/post/Post.jsx
+++ src/assets/post/Post.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
+import '../post/Post.css'
 import data from '../dataset.json';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+const thumbIcon = <FontAwesomeIcon icon={faThumbsUp} style={{ color: 'orange' }} />
+
 
 export default function Post() {
     return (
         <>
-            <div className='post'>
+            <div className='postDiv'>
                 {data.posts.map((item) => (
                     <div className='postContent' key={item.id}>
-                        <p> {item.title}</p>
+                        <div className='title'>
+                            <h2>{item.title}</h2>
+                            {thumbIcon}
+                        </div>
                         <p> {item.content}</p>
-                        <p> {item.by}</p>
-                        <p> {item.date}</p>
-                        <p> {item.read}</p>
-                        <p> {item.views}</p>
+                        <div className='foot'>
+                            <span><span className='category'>{item.category}</span> by {item.by}</span>
+                            <div>
+                                <p> {item.date} • {item.read} • {item.views}</p>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
--- src/assets/userData/UserData.jsx
+++ src/assets/userData/UserData.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import '../userData/UserData.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleCheck, faHeart, faThumbsUp, faStar, faEye } from '@fortawesome/free-regular-svg-icons'
-const verifyIcon = <FontAwesomeIcon icon={faCircleCheck} />
-const heartIcon = <FontAwesomeIcon icon={faHeart} />
-const thumbIcon = <FontAwesomeIcon icon={faThumbsUp} />
-const starIcon = <FontAwesomeIcon icon={faStar} />
-const viewIcon = <FontAwesomeIcon icon={faEye} />
+import { faCircleCheck, faHeart, faThumbsUp, faStar, faEye } from '@fortawesome/free-solid-svg-icons'
+const verifyIcon = <FontAwesomeIcon icon={faCircleCheck} style={{color: 'green'}} />
+const heartIcon = <FontAwesomeIcon icon={faHeart} style={{color: 'red'}} />
+const thumbIcon = <FontAwesomeIcon icon={faThumbsUp} style={{color: 'orange'}} />
+const starIcon = <FontAwesomeIcon icon={faStar} style={{color: 'blue'}} />
+const viewIcon = <FontAwesomeIcon icon={faEye} style={{color: 'gray'}} />
 
 export default function UserData() {
     return (
@@ -21,14 +21,23 @@ export default function UserData() {
                     </div>
                     <div>
                         <p>Anuj Gosalia {verifyIcon}</p>
-                        <button>6482</button>
-                        <button>245</button>
+                        <div className="form">
+                            <div>
+                                <input id='followers' type='button' value={"6482"} />
+                                <label htmlFor='followers'>Followers</label>
+                            </div>
+                            <div>
+                                <input id='following' type='button' value={"245"} />
+                                <label htmlFor='followers'>Following</label>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='userDataSection2'>
-                <p>Co-founder & CEO at Terribly Tiny Tales</p>
-                <a href="" target='_blank'>https://www.instagram.com/anujgosalia</a>
+                <div className='userDataSection2'>
+                    <h3>Co-founder & CEO at Terribly Tiny Tales</h3>
+                    <a href="" target='_blank'>https://www.instagram.com/anujgosalia</a>
+                    <p>{starIcon} <span>125</span> {thumbIcon} <span>12</span> {viewIcon} <span>57.8K</span> {heartIcon} <span>26.0K</span></p>
+                </div>
             </div>
         </>
     )
